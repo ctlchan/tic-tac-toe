294c96c06d3bb785fb51ef9db61d47f09e6795a2
Chris Chan <christakleungchan@gmail.com>
2023-08-17 17:58:06 -0400

    Implement win check and resulting message display

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ const gameSettings = (() => {
 
 })();
 
+
 const _SettingsFactory = (isBot, botDifficulty) => {
 
     return {isBot, botDifficulty}
@@ -56,6 +57,7 @@ const _Difficulty = (() => {
         'impossible': 1
     }
 })();
+
 
 const gameboard = (() => {
 
@@ -75,8 +77,6 @@ const gameboard = (() => {
         [undefined, undefined, undefined]
     ];
 
-
-
     return { getCell, setCell, reset}
 
 })();
@@ -85,6 +85,7 @@ const gameboard = (() => {
 const displayController = (() => {
 
     const grid = document.querySelector('.grid').children;
+    const message = document.querySelector('.message');
 
     // Update the UI based on the state of the gameboard
     const updateDisplay = () => {
@@ -106,41 +107,166 @@ const displayController = (() => {
         }
     }
 
+    // Reset the display by updating it and re-establishing click listeners - assumes the board state has been reset before
     const reset = () => {
+        message.style.visibility = 'hidden';
         updateDisplay()
         setUpListeners();
     }
 
+    // Add click listeners to grid cells
     const setUpListeners = () => {
         for (let i = 0; i < 9; i++) {
             grid[i].addEventListener('click', _makeMove);
         }
     }
 
+    // Call back function used to update the board state based on the index of the clicked cell and whose turn it is in the game
     const _makeMove = (e) => {
         let i = e.target.dataset.index;
-
         let row = Math.floor(i / 3);
         let col = i % 3;
 
-        gameboard.setCell(row, col, true); // TODO: update THIS to make move based on whose turn it is
+        gameboard.setCell(row, col, game.getPlayerOnesTurn());
         updateDisplay();
         e.target.removeEventListener('click', _makeMove);
+        game.checkWin(row, col);
+        game.toggleTurn();
+        game.incrTurnNum();
     }
 
-    return {grid, reset}
+    const displayResultMessage = (isTie) => {
+        if (isTie) {
+            message.textContent = "Game tied!";
+        }
+
+        else {
+            message.textContent = `Player ${game.getPlayerOnesTurn ? '1': '2'} wins!`
+        }
+
+        message.style.visibility = 'visible';
+    }
+
+    const messageToReset = () => {
+        let beforeText = message.textContent;
+
+        
+    }
+
+    return {grid, reset, displayResultMessage}
 })();
 
+
+// IIFE which represents and manages the current game state
 const game = (() => {
+
+    let playerOnesTurn = true;
+    let turnNumber = 1;
+
+    const getPlayerOnesTurn = () => playerOnesTurn;
+
+    const toggleTurn = () => playerOnesTurn = !playerOnesTurn;
+
+    const incrTurnNum = () => turnNumber++;
 
     const initialize = () => {
         gameboard.reset();
         displayController.reset();
     }
 
+    // Return true if the given cell lands on a winning line - intended to be called after a move is made as it checks for a win based on whose turn it is
+    const checkWin = (row, col) => {
+        let winner = false;
+
+        console.log({turnNumber, row, col})
+
+        if (turnNumber >= 5) {
+            
+            // Check row 
+            let i = 0;
+            while (i < 3 && !winner) {
+                let value = gameboard.getCell(row, i);
+
+                if (value != playerOnesTurn)
+                    break;
+
+                if (i == 2)
+                    winner = true;
+                
+                i++;
+            }
+
+            // Check column
+            i = 0;
+            while (i < 3 && !winner) {
+                let value = gameboard.getCell(i, col);
+
+                if (value != playerOnesTurn)
+                    break;
+
+                if (i == 2)
+                    winner = true;
+                
+                i++;
+            }
+
+            // Check forward diagonal (/)
+            if (row + col == 2) {
+
+                i = 0;
+                while (i < 3 && !winner) {
+                    let value = gameboard.getCell(i, 2 - i);
+
+                    if (value != playerOnesTurn)
+                        break;
+
+                    else if (i == 2)
+                        winner = true;
+                        
+                    i++
+                }
+            }
+
+            // Check backwards diagonal (\)
+            if (row == col) {
+                i = 0;
+                while (i < 3 && !winner) {
+                    let value = gameboard.getCell(i, i);
+
+                    if (value != playerOnesTurn)
+                        break;
+
+                    else if (i == 2)
+                        winner = true;
+                        
+                    i++;
+                }
+            }
+
+        }
+
+        if (winner) {
+            console.log("winner found!");
+            processWin();
+        }
+
+        if (turnNumber == 9 && !winner) {
+            // game.end('draw');  -> call message
+            displayController.displayResultMessage(true);
+        }
+    }
+
+    const processWin = () => {
+        displayController.displayResultMessage(false);
+
+        // Update message so that it acts as a button?
+    }
+
+
+
     initialize();
 
-    return {initialize}
+    return {initialize, getPlayerOnesTurn, toggleTurn, checkWin, incrTurnNum}
 
 })()
 
